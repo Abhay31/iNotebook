d4ec693b91516e1ba49689c9279dd21223543349
Abhay31 <abhaysharma0983@gmail.com>
2023-02-26 15:03:24 +0530

    Fixed Bugs

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017";
+
+const mongoURI = "mongodb://localhost:27017"
+
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to mongo successfully");
+        console.log("Connected to Mongo Successfully");
     })
 }
 
 module.exports = connectToMongo;
+
+// mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     name:{
@@ -7,7 +8,7 @@ const UserSchema = new Schema({
     },
     email:{
         type: String,
-        required: true, 
+        required: true,
         unique: true
     },
     password:{
